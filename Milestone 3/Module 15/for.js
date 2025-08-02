33498c7e66a441861d90623f88c8c8f10a3d2764
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-08-03 01:36:29 +0600

    Module 15 lesson 3

---
+++ Milestone 3/Module 15/for.js
@@ -0,0 +1,9 @@
+ const numbers = [12, 23, 345, 656, 434];
+
+ console.log(numbers);
+
+ for (const num of numbers){
+    console.log(num);
+ }
+
+ 
---
+++ Milestone 3/Module 15/while.js
@@ -0,0 +1,14 @@
+const number =  7;
+
+if (number < 10) {
+    console.log("less than 10")
+}
+console.log("Done");
+
+let num = 0;
+
+while (num < 5){
+    console.log("looping", num);
+    num++;
+}
+console.log("Loop done");
