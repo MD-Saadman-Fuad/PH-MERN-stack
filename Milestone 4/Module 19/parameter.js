5aa1ba928ef8e6fca3da74f7638151a7cc5098d7
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-08-08 16:38:20 +0600

    Milestone 4 Module 19 lesson 6 done

---
+++ Milestone 4/Module 19/parameter.js
@@ -0,0 +1,13 @@
+function eatRice(something, otherThing){
+    console.log(`Eat ${something} with ${otherThing}`)
+}
+
+eatRice('Polao', 'Coke');
+
+function square (x){
+    square = x*x;
+    return square;
+}
+
+console.log(square(3));
+
---
+++ Milestone 4/Module 19/return.js
@@ -0,0 +1,7 @@
+function TenTimes (Number){
+    const result = Number *10;
+    return result;
+}
+
+const result=TenTimes(10);
+console.log(result);
