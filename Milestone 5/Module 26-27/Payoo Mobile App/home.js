f3d5004849ecfb987252435c75b0cedf92a76da4
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-09-01 16:54:50 +0600

    Module 26 done

--- Milestone 5/Module 26-27/Payoo Mobile App/home.js
+++ Milestone 5/Module 26-27/Payoo Mobile App/home.js
@@ -1,5 +1,7 @@
 console.log("Home. js Connected")
 const validPin = 1234;
+
+// add money functionality
 document.getElementById('add-money-btn').addEventListener('click', function(event) {
     event.preventDefault();
     console.log("Add Money button clicked");
@@ -25,6 +27,7 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
         alert("Please enter a valid amount to add.");
         return;
     }
+    
     const currentBalance = parseInt(document.getElementById('available-balance').innerText);
     console.log("Current Balance:", currentBalance);
 
@@ -32,4 +35,53 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
     console.log("New Balance:", totalNewBalance);
 
     document.getElementById('available-balance').innerText = totalNewBalance;
+});
+
+//Cashout Feature
+
+document.getElementById('withdraw-money-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Withdraw Money button clicked");
+    // const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('agent-number-cashout').value;
+    const addAmount = parseInt(document.getElementById('cashout-amount').value);
+    const pinNumber = document.getElementById('pin-number-cashout').value;
+
+    // console.log("Selected Bank:", bank);
+    console.log("Account Number:", accountNumber);
+    console.log("Amount to Add:", addAmount);
+    console.log("Pin Number:", pinNumber);
+
+    if(accountNumber.length !== 11) {
+        alert("Please enter a valid 11-digit account number.");
+        return;
+    }
+    if(pinNumber != validPin) {
+        alert("Invalid PIN. Please try again.");
+        return;
+    }
+    if(isNaN(addAmount) || addAmount <= 0) {
+        alert("Please enter a valid amount to add.");
+        return;
+    }
+    
+    const currentBalance = parseInt(document.getElementById('available-balance').innerText);
+    console.log("Current Balance:", currentBalance);
+
+    const totalNewBalance = currentBalance - addAmount;
+    console.log("New Balance:", totalNewBalance);
+
+    document.getElementById('available-balance').innerText = totalNewBalance;
+});
+
+
+// toggleing feature
+document.getElementById('add-money-button').addEventListener('click', function() {
+    document.getElementById('add-money-parent').style.display = 'block';
+    document.getElementById('cash-out-parent').style.display = 'none';
+});
+
+document.getElementById('cash-out-button').addEventListener('click', function() {
+    document.getElementById('cash-out-parent').style.display = 'block';
+    document.getElementById('add-money-parent').style.display = 'none';
 });
